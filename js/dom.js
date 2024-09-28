c71484a23b37f944f7a68cbeb53d8acc25060b58
Abadat Hossain <abadat26@gmail.com>
2024-09-28 14:19:42 +0600

    dom practice

---
+++ js/dom.js
@@ -0,0 +1,16 @@
+/* const liCollection = document.getElementsByTagName('li')
+// console.log(liCollection)
+for(const li of liCollection){
+    console.log(li.innerText)
+}
+
+const title = document.getElementById('heading')
+title.innerText = 'Hello' */
+// console.log(document.body)
+
+const items= document.querySelectorAll('.container')
+console.log(item)
+for(const li of items){
+    console.log(li)
+    console.log(li)
+}
